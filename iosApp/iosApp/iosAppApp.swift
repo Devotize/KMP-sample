164510cc9e9e10a767fbfb67da4ec4b241d99e96
Denis Sychev <sychev@peterpartner.net>
2023-02-28 18:08:55 +0300

    add empty ios project in root

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Denis Sychev on 28.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
